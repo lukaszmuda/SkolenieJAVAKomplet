bf80cbc40ea68ad8a4ecee8c2862a6c6db8863af
lukaszmuda <lukas.zmuda@gmail.com>
2016-04-28 11:26:59 +0200

    added select, update

--- SkolenieJAVAKomplet/src/edu/jdbc/SelectFromTB.java
+++ SkolenieJAVAKomplet/src/edu/jdbc/SelectFromTB.java
@@ -2,11 +2,11 @@ package edu.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class VlozenieDoTB {
+public class SelectFromTB {
 
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 	static final String DB_URL = "jdbc:mysql://localhost/skola";
@@ -15,29 +15,29 @@ public class VlozenieDoTB {
 	static final String USER_PASS = "lukas";
 	
 	public static void main(String[] args) {
+		
 		Connection conn = null;
 		Statement stmt = null;
-		PreparedStatement pst = null;
 		
 		try{
 			Class.forName(JDBC_DRIVER);
+			
 			conn = DriverManager.getConnection(DB_URL, DB_USER, USER_PASS);
+			System.out.println("Pripojenie k DB skola uspesne ...");
 			
 			stmt = conn.createStatement();
-			String sql = "INSERT INTO students VALUES(1, 'Lukas', 'Zmuda', 30)";
-			//stmt.executeUpdate(sql);
-			System.out.println("Riadok 1.  vlozeny ...");
-			String sql2 = "INSERT INTO students VALUES(2, 'Jozef', 'Mrkvicka', 24)";
-			//stmt.executeUpdate(sql2);
-			System.out.println("Riadok 2 . vlozeny ...");
-			pst = conn.prepareStatement("INSERT INTO students VALUES(?, ?, ?, ?)");
-			pst.setInt(1, 3);
-			pst.setString(2, "Fero");
-			pst.setString(3, "Mravec");
-			pst.setInt(4, 55);
-			pst.executeUpdate();
+			String sql = "SELECT * FROM students";
+			ResultSet rs = stmt.executeQuery(sql);
 			
-			
+			while(rs.next()){
+				int id = rs.getInt("id");
+				String meno = rs.getString("meno");
+				String priezvisko = rs.getString(3);
+				int vek = rs.getInt(4);
+				
+				System.out.println(id + " | " + meno + " | " + priezvisko + " | " + vek);
+			}
+			System.out.println("Koniec vypisu ...");
 		}catch(SQLException se){
 			se.printStackTrace();
 		}catch(Exception e){
@@ -46,16 +46,18 @@ public class VlozenieDoTB {
 			try{
 				if(conn != null)
 					conn.close();
-			}catch(SQLException see){
-				see.printStackTrace();
+			}catch(SQLException se2){
+				se2.printStackTrace();
 			}
 			try{
 				if(stmt != null)
 					stmt.close();
-			}catch(SQLException seee){
-				seee.printStackTrace();
+			}catch(SQLException se3){
+				se3.printStackTrace();
 			}
 		}
+		
+		System.out.println("Koniec programu");
 
 	}
 
--- SkolenieJAVAKomplet/src/edu/jdbc/UpdateVTB.java
+++ SkolenieJAVAKomplet/src/edu/jdbc/UpdateVTB.java
@@ -2,47 +2,51 @@ package edu.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class VlozenieDoTB {
-
+public class UpdateVTB {
+	
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 	static final String DB_URL = "jdbc:mysql://localhost/skola";
 	
 	static final String DB_USER = "root";
 	static final String USER_PASS = "lukas";
-	
+
 	public static void main(String[] args) {
 		Connection conn = null;
 		Statement stmt = null;
-		PreparedStatement pst = null;
 		
 		try{
 			Class.forName(JDBC_DRIVER);
+			
 			conn = DriverManager.getConnection(DB_URL, DB_USER, USER_PASS);
 			
 			stmt = conn.createStatement();
-			String sql = "INSERT INTO students VALUES(1, 'Lukas', 'Zmuda', 30)";
-			//stmt.executeUpdate(sql);
-			System.out.println("Riadok 1.  vlozeny ...");
-			String sql2 = "INSERT INTO students VALUES(2, 'Jozef', 'Mrkvicka', 24)";
-			//stmt.executeUpdate(sql2);
-			System.out.println("Riadok 2 . vlozeny ...");
-			pst = conn.prepareStatement("INSERT INTO students VALUES(?, ?, ?, ?)");
-			pst.setInt(1, 3);
-			pst.setString(2, "Fero");
-			pst.setString(3, "Mravec");
-			pst.setInt(4, 55);
-			pst.executeUpdate();
 			
+			String sql = "UPDATE students SET vek = 31 WHERE id = 1";
 			
+			stmt.executeUpdate(sql);
+			
+			sql = "SELECT * FROM students";
+			
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			while(rs.next()){
+				int id = rs.getInt("id");
+				String meno = rs.getString("meno");
+				String priezvisko = rs.getString(3);
+				int vek = rs.getInt(4);
+				
+				System.out.println(id + " | " + meno + " | " + priezvisko + " | " + vek);
+			}
 		}catch(SQLException se){
 			se.printStackTrace();
 		}catch(Exception e){
 			e.printStackTrace();
-		}finally{
+		}
+		finally{
 			try{
 				if(conn != null)
 					conn.close();
@@ -52,8 +56,8 @@ public class VlozenieDoTB {
 			try{
 				if(stmt != null)
 					stmt.close();
-			}catch(SQLException seee){
-				seee.printStackTrace();
+			}catch(SQLException sss){
+				sss.printStackTrace();
 			}
 		}
 
